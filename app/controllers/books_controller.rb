5a121d2eab773b7c3484b3a8dc5d530880e011da
EC2 Default User <ec2-user@ip-172-31-21-33.ec2.internal>
2020-10-28 16:24:53 +0000

    [Update]3-8 conntroller,html記載

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,33 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.params([:id])
+    
+  end
+  def destroy
+    book = Book.find(params[:id])
+    
+  end
+end
+
+# ストロングパラメータ
+private
+def book_params
+  params.require(:book).permit(:title,:body)
+end
